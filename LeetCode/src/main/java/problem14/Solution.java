c0daea1f030ce5063ffee0898d5016305f097c4e
brucebat <brucewatchbatman@163.com>
2021-03-01 23:32:23 +0800

    pom(LeetCode): 修改maven打包依赖jdk版本

---
+++ LeetCode/src/main/java/problem14/Solution.java
@@ -0,0 +1,26 @@
+package problem14;
+
+import java.util.Objects;
+
+/**
+ * LeetCode:14 最长公共前缀
+ *
+ * @author suntianyu
+ * @version 1.0
+ * @since Created at 2021/3/1 11:08 下午
+ */
+public class Solution {
+
+    /**
+     * 根据给出的字符串数组判断最长公共前缀
+     *
+     * @param strs 待判断字符串数组
+     * @return 最长公共前缀
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if (Objects.isNull(strs)) {
+
+        }
+        return null;
+    }
+}
