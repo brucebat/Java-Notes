f9483a78068b31e979991e828b1033a145be2aed
brucebat <brucewatchbatman@163.com>
2021-03-03 21:22:59 +0800

    feat(LeetCode): 新增LeetCode14题的题解

--- LeetCode/src/main/java/problem14/Solution.java
+++ LeetCode/src/main/java/problem14/Solution.java
@@ -1,5 +1,6 @@
 package problem14;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 /**
@@ -17,10 +18,25 @@ public class Solution {
      * @param strs 待判断字符串数组
      * @return 最长公共前缀
      */
-    public String longestCommonPrefix(String[] strs) {
-        if (Objects.isNull(strs)) {
-
+    public static String longestCommonPrefix(String[] strs) {
+        if (Objects.isNull(strs) || strs.length == 0) {
+            return "";
         }
-        return null;
+        for (int i = 1; i <= strs[0].length(); i++) {
+            String tmpPrefix = strs[0].substring(0, i);
+            boolean checkLimit = false;
+            for (String str : strs) {
+                if (!str.startsWith(tmpPrefix)) {
+                    return tmpPrefix.length() == 1 ? "" : tmpPrefix.substring(0, tmpPrefix.length() - 1);
+                }
+                if (tmpPrefix.length() == str.length()) {
+                    checkLimit = true;
+                }
+            }
+            if (checkLimit) {
+                return tmpPrefix;
+            }
+        }
+        return "";
     }
 }
