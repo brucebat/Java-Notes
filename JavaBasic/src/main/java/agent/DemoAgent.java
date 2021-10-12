e0d6cf4426834fef999ebf24e90b4cb50380d50a
brucebat <brucewatchbatman@163.com>
2021-10-13 00:37:56 +0800

    feat(*): 修改方法打印的参数

--- JavaBasic/src/main/java/agent/DemoAgent.java
+++ JavaBasic/src/main/java/agent/DemoAgent.java
@@ -21,9 +21,8 @@ public class DemoAgent {
      */
     public static void premain(String agentArgs, Instrumentation instrumentation) {
         System.out.println("premian: 这是一个实验用的DemoAgent");
-        System.out.println("premian: " + Thread.currentThread().getThreadGroup().getName() + ", threadId: " + Thread.currentThread().getId());
+        System.out.println("premian: " + Thread.currentThread().getName() + ", threadId: " + Thread.currentThread().getId());
         System.out.println("premian, 当前线程是否为保护线程: " + Thread.currentThread().isDaemon());
-        instrumentation.addTransformer(new DefineTransformer());
 
     }
 
@@ -48,8 +47,6 @@ public class DemoAgent {
                 return classfileBuffer;
             }
             System.out.println("premain load Class:" + className);
-            // 这里获取的编译后的二进制文件，通过修改/替换对应的二进制文件我们可以对原有的类新增一些新的功能
-//            System.out.println("premain load Class filer: " + new String(classfileBuffer));
             // 这里只是提供了一个入口可以去感知到对应的class文件，如果想要对class文件进行实际的变更可以使用javaassist或者Byte Code等工具，也可以直接针对上面的字节数据进行修改
             return classfileBuffer;
         }
