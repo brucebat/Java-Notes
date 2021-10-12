c99c84116cf9d2c55c5cc43f9c6108c5c0234a60
brucebat <brucewatchbatman@163.com>
2021-10-12 21:45:49 +0800

    feat(*): 新增注释

--- DesignPattern/src/main/java/TestAttach.java
+++ DesignPattern/src/main/java/TestAttach.java
@@ -15,8 +15,10 @@ public class TestAttach {
     public static void main(String[] args) throws IOException, AttachNotSupportedException {
         List<VirtualMachineDescriptor> virtualMachineDescriptors = VirtualMachine.list();
         for (VirtualMachineDescriptor virtualMachineDescriptor : virtualMachineDescriptors) {
+            System.out.println("vm displayName : " + virtualMachineDescriptor.displayName());
             if (virtualMachineDescriptor.displayName().equals("App")) {
                 System.out.println(virtualMachineDescriptor.id());
+                // 这里最终要的一点就是不需要通过使用命令行的方式去指定需要监测的JVM进程, 可以通过较为明确的虚拟机名称来进行设置
                 VirtualMachine virtualMachine = VirtualMachine.attach(virtualMachineDescriptor.id());
                 try {
                     virtualMachine.loadAgent("JavaBasic/target/JavaBasic-1.0.0-SNAPSHOT-jar-with-dependencies.jar");
--- JavaBasic/src/main/java/agent/DemoAgent.java
+++ JavaBasic/src/main/java/agent/DemoAgent.java
@@ -23,6 +23,7 @@ public class DemoAgent {
         System.out.println("premian: 这是一个实验用的DemoAgent");
         System.out.println("premian: " + Thread.currentThread().getThreadGroup().getName() + ", threadId: " + Thread.currentThread().getId());
         System.out.println("premian, 当前线程是否为保护线程: " + Thread.currentThread().isDaemon());
+        instrumentation.addTransformer(new DefineTransformer());
 
     }
 
@@ -39,7 +40,17 @@ public class DemoAgent {
 
         @Override
         public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+            // classfileBuffer
+            if (null == className || 0 == className.length()) {
+                return classfileBuffer;
+            }
+            if (!className.contains("App")) {
+                return classfileBuffer;
+            }
             System.out.println("premain load Class:" + className);
+            // 这里获取的编译后的二进制文件，通过修改/替换对应的二进制文件我们可以对原有的类新增一些新的功能
+//            System.out.println("premain load Class filer: " + new String(classfileBuffer));
+            // 这里只是提供了一个入口可以去感知到对应的class文件，如果想要对class文件进行实际的变更可以使用javaassist或者Byte Code等工具，也可以直接针对上面的字节数据进行修改
             return classfileBuffer;
         }
     }
