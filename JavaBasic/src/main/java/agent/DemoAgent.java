0b4bc17d38245d2cee4610964b3b224f8524b7b0
brucebat <brucewatchbatman@163.com>
2021-10-13 18:14:55 +0800

    feat(*): 修改方法当中的打印信息逻辑

--- JavaBasic/src/main/java/agent/DemoAgent.java
+++ JavaBasic/src/main/java/agent/DemoAgent.java
@@ -23,13 +23,14 @@ public class DemoAgent {
         System.out.println("premian: 这是一个实验用的DemoAgent");
         System.out.println("premian: " + Thread.currentThread().getName() + ", threadId: " + Thread.currentThread().getId());
         System.out.println("premian, 当前线程是否为保护线程: " + Thread.currentThread().isDaemon());
-
+        instrumentation.addTransformer(new DefineTransformer("premain"));
     }
 
     public static void agentmain(String agentArgs, Instrumentation instrumentation) {
         System.out.println("agnetmain: 这是一个实验用的DemoAgent, 线程名称为: " + Thread.currentThread().getName());
         System.out.println(Thread.currentThread().getThreadGroup().getName() + ", threadId: " + Thread.currentThread().getId());
         System.out.println("当前线程是否为保护线程: " + Thread.currentThread().isDaemon());
+        instrumentation.addTransformer(new DefineTransformer("agentmain"));
     }
 
     /**
@@ -37,17 +38,19 @@ public class DemoAgent {
      */
     static class DefineTransformer implements ClassFileTransformer {
 
+        private final String name;
+
+        public DefineTransformer(String name) {
+            this.name = name;
+        }
+
         @Override
         public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
-            // classfileBuffer
-            if (null == className || 0 == className.length()) {
-                return classfileBuffer;
-            }
-            if (!className.contains("App")) {
-                return classfileBuffer;
-            }
-            System.out.println("premain load Class:" + className);
-            // 这里只是提供了一个入口可以去感知到对应的class文件，如果想要对class文件进行实际的变更可以使用javaassist或者Byte Code等工具，也可以直接针对上面的字节数据进行修改
+            System.out.println(name + " load Class:" + className + ", ThreadName: " + Thread.currentThread().getName() +
+                    ", ThreadGroupName: " + Thread.currentThread().getThreadGroup().getName() + ", 是否为保护线程: " + Thread.currentThread().isDaemon());
+            // 这里只是提供了一个入口可以去感知到对应的class文件，
+            // 如果想要对class文件进行实际的变更可以使用javaassist或者Byte Code等工具，
+            // 也可以直接针对上面的字节数据进行修改
             return classfileBuffer;
         }
     }
