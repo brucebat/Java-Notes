8109b5e3d78d56c1fd5cb31b466117d4ca704b9a
brucebat <brucewatchbatman@163.com>
2022-02-21 10:09:17 +0800

    feat(src): 新增关于字符流相关方法调用逻辑编写

--- JavaTest/src/main/java/io/InputStreamTest.java
+++ JavaTest/src/main/java/io/InputStreamTest.java
@@ -33,16 +33,51 @@ public class InputStreamTest {
      * 测试字节流数组读取
      */
     public static void testByteArrayInputStream() {
-        String data = "this is a test.";
+        String data = "测试";
         try (InputStream inputStream = new ByteArrayInputStream(data.getBytes())) {
             // 将输入流中的第一个字节数据读取到缓冲区中
             int read = inputStream.read();
             // 如果read返回的值小于0，表示读取完毕
             while (read != -1) {
                 // 对读取到的数据进行处理
-                System.out.println((char) read);
+                System.out.print((char) read);
                 // 继续读取下一个字节
                 read = inputStream.read();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void testCharArrayReader() {
+        String text = "蝙蝠侠";
+        // 创建一个字符输入流，这里使用try-with-resources来避免显示的关闭流
+        try (Reader reader = new CharArrayReader(text.toCharArray())) {
+            // 将字符输入流中的第一个字符读取到缓冲区中
+            int read = reader.read();
+            // 如果read返回的值小于0，表示读取完毕
+            while (read != -1) {
+                // 对读取到的数据进行处理
+                System.out.print((char) read);
+                // 继续读取下一个字符
+                read = reader.read();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void testFileReader() {
+        // 创建一个字符输入流，这里使用try-with-resources来避免显示的关闭流
+        try (Reader reader = new FileReader("/Users/suntianyu/Desktop/test.json")) {
+            // 将字符输入流中的第一个字符读取到缓冲区中
+            int read = reader.read();
+            // 如果read返回的值小于0，表示读取完毕
+            while (read != -1) {
+                // 对读取到的数据进行处理
+                System.out.print((char) read);
+                // 继续读取下一个字符
+                read = reader.read();
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -66,7 +101,20 @@ public class InputStreamTest {
         }
     }
 
+    public static void testFileWriter() {
+        String text = "蝙蝠侠";
+        // 创建一个字符输出流，这里使用try-with-resources来避免显示的关闭流
+        try (Writer writer = new FileWriter("/Users/suntianyu/Desktop/test.json")) {
+            // 将字符输入流中的第一个字符读取到缓冲区中
+            writer.write(text);
+            // 将缓冲区中的数据写入到输出流中
+            writer.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        standardOutPutStream();
+        testFileReader();
     }
 }
